cace4d78c9cd40c6534df301c9deaef23a910c1e
imdoingotherthings <asaiandrade@yahoo.com>
2017-01-08 18:53:40 -0800

    Creating new rooms

--- app/controllers/rooms.js
+++ app/controllers/rooms.js
@@ -3,8 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     newChatRoom: function () {
+      var room = this.getProperties("room")
       var newRoom = this.store.createRecord('room', {
-        name: this.get('name')
+        name: room
       });
       newRoom.save();
     }
