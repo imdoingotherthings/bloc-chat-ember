55d15387af2923de22465b454ed4b3d518f0275c
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-01-11 18:33:18 -0800

    fix problems

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,4 @@
-import Ember from 'ember'
+import Ember from 'ember';
 import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 const { inject } = Ember;
--- app/controllers/rooms.js
+++ app/controllers/rooms.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     newChatRoom: function () {
-      var room = this.getProperties('room')
+      // var room = this.getProperties('room')
       var newRoom = this.store.createRecord('room', {
         name: this.get('name')
       });
