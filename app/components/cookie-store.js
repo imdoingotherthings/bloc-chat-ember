790d8ca994b4e4a304eb394e8819e6a5e79dd8aa
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-01-20 07:52:53 -0800

    why are there errors?

--- app/components/cookie-store.js
+++ app/components/cookie-store.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   cookieMonster: Ember.inject.service(),
 
-  userName: Ember.computed.reads('cookieMonster.cookies.userName')
+  userName: Ember.computed.reads('cookieMonster.cookies.userName'),
 
-  allCookies: Ember.computed.('cookieMonster.cookies', function() {
+  allCookies: Ember.computed('cookieMonster.cookies', function() {
     return JSON.stringify(this.get('cookieMonster.cookies'));
   })
 });
