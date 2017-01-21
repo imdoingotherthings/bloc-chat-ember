fc106d929abefa5c4ff5e866632d45a9b68914c8
imdoingotherthings <asaiandrade@yahoo.com>
2017-01-21 14:41:34 -0800

    cleaning

--- app/components/cookie-store.js
+++ app/components/cookie-store.js
@@ -3,9 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   cookieMonster: Ember.inject.service(),
 
-  userName: Ember.computed.reads('cookieMonster.cookies.userName')
+  userName: Ember.computed.reads('cookieMonster.cookies.userName'),
 
-  allCookies: Ember.computed.('cookieMonster.cookies', function() {
+  userAvailable: Ember.computed('cookieMonster.cookies.userName', function () {
+    var someUser = this.get('cookieMonster.cookies.userName');
+    // This is where the modal goes.
+    // If the cookies show that a user has been here, do not display the modal.
+    if (someUser === ) {
+      return "yes";
+    } else
+  }
+
+  allCookies: Ember.computed('cookieMonster.cookies', function() {
     return JSON.stringify(this.get('cookieMonster.cookies'));
   })
 });
